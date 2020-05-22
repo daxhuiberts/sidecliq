adb5c623c009434a0bceec309dad55491348aaac
Dax Huiberts <daxhuiberts@gmail.com>
2020-05-22 16:10:52 +0200

    Remove inner connection

--- sidemon-lib/src/client.rs
+++ sidemon-lib/src/client.rs
@@ -1,19 +1,21 @@
 use itertools::Itertools;
+use redis::Commands;
 use std::collections::HashMap;
-use super::connection::Connection;
 use super::types::*;
 
 pub struct Client {
-    connection: Connection,
+    inner: redis::Connection,
 }
 
 impl Client {
     pub fn new(url: &str) -> Result<Self> {
-        Ok(Self { connection: Connection::new(url)? })
+        let client = redis::Client::open(url)?;
+        let conn = client.get_connection()?;
+        Ok(Self { inner: conn })
     }
 
     pub fn process_names(&mut self) -> Result<Vec<String>> {
-        Ok(serde_redis::from_redis_value(self.connection.process_names()?)?)
+        Ok(self.inner.smembers("processes")?)
     }
 
     pub fn process(&mut self, process_name: &str) -> Result<Process> {
@@ -28,7 +30,7 @@ impl Client {
             pub beat: f64
         }
 
-        let mut process: ProcessRaw = serde_redis::from_redis_value(self.connection.process(process_name)?)?;
+        let mut process: ProcessRaw = serde_redis::from_redis_value(self.inner.hgetall(process_name)?)?;
         let info = std::mem::take(&mut process.info);
         let mut map = if let JsonValue::Object(map) = info { Ok(map) } else { Err("process.info is not a json object") }?;
         map.insert("busy".into(), JsonValue::Number(process.busy.into()));
@@ -38,32 +40,32 @@ impl Client {
     }
 
     pub fn workers(&mut self, process_name: &str) -> Result<HashMap<String, Job>> {
-        let raw_result: HashMap<String, String> = serde_redis::from_redis_value(self.connection.workers(&format!("{}:workers", process_name))?)?;
+        let raw_result: HashMap<String, String> = self.inner.hgetall(format!("{}:workers", process_name))?;
         let result: Result<HashMap<String, Job>> = raw_result.into_iter().map(|(id, worker)| Ok((id, serde_json::from_str(&worker)?)) ).try_collect();
         result
     }
 
     pub fn queue_names(&mut self) -> Result<Vec<String>> {
-        Ok(serde_redis::from_redis_value(self.connection.queue_names()?)?)
+        Ok(self.inner.smembers("queues")?)
     }
 
     pub fn queue(&mut self, queue_name: &str) -> Result<Vec<Job>> {
-        let raw_result: Vec<String> = serde_redis::from_redis_value(self.connection.queue(queue_name)?)?;
+        let raw_result: Vec<String> = self.inner.lrange(format!("queue:{}", queue_name), 0, 10)?;
         Ok(raw_result.iter().map(AsRef::as_ref).map(serde_json::from_str).try_collect()?)
     }
 
     pub fn retry(&mut self) -> Result<Vec<Job>> {
-        let raw_result: Vec<String> = serde_redis::from_redis_value(self.connection.retry()?)?;
+        let raw_result: Vec<String> = self.inner.zrange("retry", 0, 10)?;
         Ok(raw_result.iter().map(AsRef::as_ref).map(serde_json::from_str).try_collect()?)
     }
 
     pub fn schedule(&mut self) -> Result<Vec<Job>> {
-        let raw_result: Vec<String> = serde_redis::from_redis_value(self.connection.schedule()?)?;
+        let raw_result: Vec<String> = self.inner.zrange("schedule", 0, 10)?;
         Ok(raw_result.iter().map(AsRef::as_ref).map(serde_json::from_str).try_collect()?)
     }
 
     pub fn dead(&mut self) -> Result<Vec<Job>> {
-        let raw_result: Vec<String> = serde_redis::from_redis_value(self.connection.dead()?)?;
+        let raw_result: Vec<String> = self.inner.zrange("dead", 0, 10)?;
         Ok(raw_result.iter().map(AsRef::as_ref).map(serde_json::from_str).try_collect()?)
     }
 }
--- sidemon-lib/src/lib.rs
+++ sidemon-lib/src/lib.rs
@@ -1,3 +1,2 @@
-mod connection;
 pub mod client;
 pub mod types;
